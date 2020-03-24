244b892f7a975089fd77c6deb96780978e6abb9d
sanchit48 <sanchitkhare3@gmail.com>
2020-03-24 14:57:28 +0530

    Shows pictures of fav food

---
+++ lib/screens/favorites_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
+
+class FavoritesScreen extends StatelessWidget {
+
+  final List<Meal> favoriteMeals;
+
+  FavoritesScreen(this.favoriteMeals);
+
+  @override
+  Widget build(BuildContext context) {
+
+    if(favoriteMeals.isEmpty) {
+
+      return Center(child: Text('You have no favorites yet - Start adding some'));
+
+    }
+
+    else {
+
+      return ListView.builder(
+
+        itemBuilder: (ctx, index) {
+
+          return MealItem(id: favoriteMeals[index].id, title: favoriteMeals[index].title, imageUrl: favoriteMeals[index].imageUrl, duration: favoriteMeals[index].duration, complexity: favoriteMeals[index].complexity, affordability: favoriteMeals[index].affordability);
+
+        },
+
+        itemCount: favoriteMeals.length,
+
+      );
+
+    }
+
+  }
+
+}
